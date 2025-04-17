d7358cc09b721ff4c28ae8f2033a481a17e51a41
samaarr <samar1patil@gmail.com>
2025-04-17 16:51:36 +0100

    frontend setup

--- main.py
+++ main.py
@@ -1,11 +1,40 @@
 from fastapi import FastAPI
+import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from typing import List
+
+class Fruit(BaseModel):
+    name: str
+   
+class Fruits(BaseModel):
+    fruits: List[Fruit]
+
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message": "Hello, World!"}
+origins = [
+    "http://localhost:3000",
+]
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+memory_db = {'fruits': []}
+
+@app.get("/fruits", response_model=Fruits)
+def get_fruits():
+    return Fruits(fruits=memory_db['fruits'])
+
+@app.post("/fruits", response_model=Fruit)
+def add_fruit(fruit: Fruit):
+    memory_db['fruits'].append(fruit)
+    return fruit
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
